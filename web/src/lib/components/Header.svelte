aa3fdd31c4f5df64f35d94714127e515fdd1cb17
Paul Lavender-Jones <paul@lavender-jones.com>
2021-09-08 00:24:07 +0100

    🩹 Added back button

--- web/src/lib/components/Header.svelte
+++ web/src/lib/components/Header.svelte
@@ -1,34 +1,23 @@
 <script lang="ts">
   import Toggle from '$lib/components/DarkModeToggle.svelte';
   import { page } from '$app/stores';
+  import Arrow from '@svicons/remix-line/arrow-left.svelte';
 
-  // Put routes in this object in the form -> Name: 'href'
-  const routes = {
-    'Home': '/',
-    'Projects': '/projects',
-    'Posts': '/posts',
-  };
+  $: currentPath = $page.path.substr(0, $page.path.lastIndexOf('/')) || $page.path;
 </script>
 
-<div class="flex flex-row justify-between w-full py-4">
+<div class="flex flex-row justify-between w-full py-4 items-center">
   <nav class="flex flex-row space-x-4">
-    <!-- {#each Object.entries(routes) as [name, href] (name) }
-      <div class="text-lg sm:text-base">
-        <a {href} sveltekit:prefetch class="text-gray-500 transition-shadow hover:text-green-800 dark:hover:text-green-100 shadow-thin hover:shadow-thick ring-green-500 dark:ring-opacity-40 ring-opacity-25 dark:text-gray-400" class:selected={currentPath === href}>{name}</a>
+    {#if currentPath !== '/'}
+     <div class="text-lg sm:text-base">
+        <a href="/" sveltekit:prefetch class="transition-shadow shadow-thin hover:shadow-thick ring-green-500 dark:ring-opacity-40 ring-opacity-25">
+          <Arrow class="inline" height="1rem"></Arrow>
+          Back
+        </a>
       </div>
-    {/each} -->
+    {/if}
   </nav>
   <div class="no-js-hidden">
     <Toggle />
   </div>
 </div>
-
-<style>
-  .selected {
-    @apply text-gray-900;
-  }
-
-  html.dark * .selected {
-    @apply text-gray-200;
-  }
-</style>
