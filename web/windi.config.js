51e9efcbd28b0e666f762d0295ceeeaa1a513366
Paul Lavender-Jones <paul@lavender-jones.com>
2021-09-08 23:01:42 +0100

    🩹 Fixed windi prose colours

--- web/windi.config.js
+++ web/windi.config.js
@@ -17,31 +17,28 @@ export default defineConfig({
               textDecoration: 'underline',
               textDecorationStyle: 'dotted',
             },
-            abbr: {
-              textDecorationThickness: '2px',
-            },
-            '.footnotes': {
-              fontSize: '0.575rem',
-              fontFamily: 'monospace',
+            blockquote: {
+              borderLeftColor: colors.gray[300],
+              fontStyle: 'normal',
+              borderOpacity: 0.5
             },
           },
         },
         dark: {
           css: {
             color: colors.gray[300],
-            "ul > li::before": { backgroundColor: colors.gray[700] },
+            "ul > li::before": { backgroundColor: colors.gray[600] },
+            strong: { color: colors.gray[100] },
             hr: { borderColor: colors.gray[800] },
             blockquote: {
-              color: colors.gray[100],
-              borderLeftColor: colors.gray[800],
+              color: colors.gray[200],
+              borderLeftColor: colors.gray[700],
             },
             h1: { color: colors.gray[100] },
             h2: { color: colors.gray[100] },
             h3: { color: colors.gray[100] },
             h4: { color: colors.gray[100] },
-            a: {
-              color: colors.green[200],
-            },
+            a: { color: colors.green[200] },
           },
         },
       },
