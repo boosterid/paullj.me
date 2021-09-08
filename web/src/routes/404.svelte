ae4c2df919e5ec63729db8895c5149eb56e1b7f8
Paul Lavender-Jones <paul@lavender-jones.com>
2021-09-08 10:54:22 +0100

    🥅 Add custom 404 page

---
+++ web/src/routes/404.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+  <title>Page not found | Paul Lavender-Jones</title>
+</svelte:head>
+
+<div class="mt-48">
+  <h1 class="font-black text-9xl">404.</h1>
+  <p class="font-serif mt-4 text-xl">Sorry, couldn't find the page you were looking for!</p>
+</div>
